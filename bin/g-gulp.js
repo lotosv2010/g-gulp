8c64d3d0f0481c82d2685ddcd524787e00db99d1
gaowenbin <lotosv2010@163.com>
2022-05-26 00:12:28 +0800

    feat(demo): add demo

--- bin/g-gulp.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-process.argv.push('--cwd');
-process.argv.push(process.cwd());
-process.argv.push('--gulpfile');
-// process.argv.push(require.resolve('../lib/index.js'));  // 等价下面
-process.argv.push(require.resolve('..'));
-
-require('gulp/bin/gulp')
